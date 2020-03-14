15ed0c431ac46de1c89cc06573ee1e65dbe10bed
pjack5 <pjack5@vt.edu>
2020-03-14 10:52:46 -0400

    updated extras.py file

--- learning_templates/basic_app/templatetags/my_extras.py
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -3,10 +3,10 @@ from django import template
 register = template.Library()
 
 def cut(value,arg):
-    """"
+    """
     This cuts out all values of "arg" from the string!
 
-    """"
+    """
 
     #passing what we're looking for, and replacing it with the second argument below
     return value.replace(arg,'')
