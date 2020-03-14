c92ff55b03238c833ca1d9dd80f31493fb65f8fa
pjack5 <pjack5@vt.edu>
2020-03-14 09:43:51 -0400

    'test'

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
+
+Adding Test Comment
 """
 
 import os
